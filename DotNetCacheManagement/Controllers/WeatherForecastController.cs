20506f43aa049e0d5a8f915e0aa22897a34e9750
mehedihasan9339 <mehedihasan9339@gmail.com>
2024-07-05 16:11:08 +0600

    first commit

---
+++ DotNetCacheManagement/Controllers/WeatherForecastController.cs
@@ -0,0 +1,83 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace DotNetCacheManagement.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IMemoryCache _memoryCache;
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IMemoryCache memoryCache)
+        {
+            _logger = logger;
+            _memoryCache = memoryCache;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        [ResponseCache(Duration = 10)]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+
+        [HttpGet(Name = "GetWeatherForecastMemoryCaching")]
+        public ActionResult<WeatherForecastsDto> GetWeatherForecastMemoryCaching()
+        {
+            //Cache Key
+            const string cacheKey = "weatherResult";
+
+            if (!_memoryCache.TryGetValue(cacheKey, out IEnumerable<WeatherForecast> forecast))
+            {
+                forecast = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                }).Take(2)
+                .ToArray();
+
+                var cacheOptions = new MemoryCacheEntryOptions()
+                {
+                    AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(20),
+                    SlidingExpiration = TimeSpan.FromSeconds(10)
+                };
+
+                _memoryCache.Set(cacheKey, forecast, cacheOptions);
+            }
+
+
+
+            var weather2 = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            }).Take(2)
+            .ToArray();
+
+            var result = new WeatherForecastsDto
+            {
+                Weather1 = forecast,
+                Weather2 = weather2
+            };
+
+            return Ok(result);
+        }
+
+
+    }
+}
---
+++ DotNetCacheManagement/WeatherForecastsDto.cs
@@ -0,0 +1,8 @@
+﻿namespace DotNetCacheManagement
+{
+    public class WeatherForecastsDto
+    {
+        public IEnumerable<WeatherForecast> Weather1 { get; set; }
+        public IEnumerable<WeatherForecast> Weather2 { get; set; }
+    }
+}
